6ff783fadfd0d0d2f9e14a7d54d8aa72bc7bc9fc
Daniel Visoiu <daniel.visoiu@toptal.com>
2021-02-05 00:04:38 +0200

    Added wcc-for samples inside About page.

--- scripts/controllers/AboutController.js
+++ scripts/controllers/AboutController.js
@@ -10,7 +10,83 @@ class AboutController extends WccController {
         },
         conditionResult: true,
         wasSubmitted: false,
-        hasErrors: false
+        hasErrors: false,
+        itemsOne: [
+            {
+                innerHTML: "Item 0",
+            },
+            {
+                innerHTML: "Item 1",
+            },
+            {
+                innerHTML: "Item 2",
+            },
+            {
+                innerHTML: "Item 3",
+            },
+        ],
+        itemsTwo: [
+            {
+                title: {
+                    innerText: "Title 1",
+                    class: "title--bigger",
+                    style: "color: red",
+                },
+                content: {
+                    innerText: "Description 1",
+                },
+            },
+            {
+                title: {
+                    innerText: "Title 2",
+                },
+                content: {
+                    innerText: "Description 2",
+                },
+            },
+            {
+                title: {
+                    innerHTML: 'Title 3 <a href="/more">...read more</a>',
+                },
+                content: {
+                    innerText: "Description 3",
+                },
+            },
+        ],
+        itemsThree: [
+            {
+                div: { innerText: "A" },
+                array: [{ innerText: "a1" }, { innerText: "a2" }],
+            },
+            {
+                div: { innerText: "B" },
+                array: [{ innerText: "b1" }, { innerText: "b2" }, { innerText: "b3" }],
+            },
+        ],
+        addItem: {
+            innerText: "Add +",
+            element: true,
+        },
+        removeItem: {
+            innerText: "Remove -",
+            element: true,
+        },
+        modifyItem: {
+            innerText: `What's your lucky number?`,
+            element: true,
+        },
+        reverse: {
+            innerText: `Reverse ↻`,
+            element: true,
+        },
+        button1: {
+            innerText: "Button 1",
+            data: "button1 data",
+        },
+        button2: {
+            innerText: "Button 1",
+            data: "button2 data"
+        },
     });
 
     constructor(element, history) {
@@ -38,14 +114,14 @@ class AboutController extends WccController {
         const updateSubmittedInfo = () => {
             setTimeout(() => {
                 this.model.wasSubmitted = true;
-                
+        
                 setTimeout(() => {
                     this.model.hasErrors = true;
-                    
+        
                     setTimeout(() => {
                         this.model.wasSubmitted = false;
                         this.model.hasErrors = false;
-
+        
                         updateSubmittedInfo();
                     }, 1000)
                 }, 1000);
@@ -53,16 +129,62 @@ class AboutController extends WccController {
         };
         updateSubmittedInfo();
 
-
-        this.element.querySelector('button[name="modal"]').addEventListener("click", () => {
+        this.onTagClick('showModal', (model, event) => {
             this.showModal("Sample content");
         });
-        this.element.querySelector('button[name="modal-error"]').addEventListener("click", () => {
+        this.onTagClick('showModalError', (model, event) => {
             this.showErrorModal("Error message", "Error");
         });
-        this.element.querySelector('button[name="modal-redirect"]').addEventListener("click", () => {
-            this.showErrorModalAndRedirect("Error message", "contact", 3000);
+        this.onTagClick('showModalRedirect', (model, event) => {
+            this.showErrorModalAndRedirect("Error message", "Redirecting to contact", "contact", 3000);
         });
+
+        this.model.addItem.element.addEventListener("click", () => {
+            let index = this.model.itemsTwo.length + 1;
+            this.model.itemsTwo.push({
+                title: { innerText: `Title ${index}` },
+                content: { innerText: `Span ${index}` },
+            });
+        });
+
+        this.model.removeItem.element.addEventListener("click", () => {
+            this.model.itemsTwo.splice(-1, 1);
+        });
+
+        this.model.modifyItem.element.addEventListener("click", () => {           
+            this.model.itemsTwo[0] = {
+                innerText: `My lucky number is ${Math.round(Math.random() * 100)}!`
+            };
+        });
+
+        this.model.reverse.element.addEventListener("click", () => {
+            this.model.itemsTwo.reverse();
+        });
+
+        const sample1TagSecondListener = (model, event) => {
+            console.log(`2) Tag sample1`, model, event);
+        };
+
+        this.onTag('sample1', 'click', (model, event) => {
+            console.log(`1) Tag sample1`, model, event);
+        });
+        this.onTagClick('sample1', sample1TagSecondListener);
+        this.onTagClick('sample1', (model, event) => {
+            console.log(`3) Tag sample1`, model, event);
+        });
+
+        this.onTagClick('button1', (model, event) => {
+            console.log(`Tag button1 click`, model, event);
+            alert(`Tag button1 click with data: ${model.data}`);
+        });
+        this.onTagClick('button2', (model, event) => {
+            console.log(`Tag button2 click`, model, event);
+            alert(`Tag button2 click with data: ${model.data}`);
+        });
+
+        setTimeout(() => {
+            this.offTagClick('sample1', sample1TagSecondListener);
+        }, 2000)
     }
 }
 
