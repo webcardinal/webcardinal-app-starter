b0a3fd583d5ef4a484e9550b5164df955afdd5d7
Daniel Visoiu <daniel.visoiu@toptal.com>
2021-02-17 23:18:37 +0200

    Fixed modal content issue. Added webc-template usages to webc-page page.

--- scripts/controllers/WebcPageController.js
+++ scripts/controllers/WebcPageController.js
@@ -29,6 +29,24 @@ class WebcPageController extends WebcController {
                 html: this.t("Item 3"),
             },
         ],
+        templateInput: {
+            type: "text",
+            value: 0,
+            style: "border: 1px solid #333",
+        },
+        templateLabel: {
+            text: this.t("templateLabel"),
+        },
+        templateInnerInput: {
+            templateInput: {
+                type: "text",
+                value: 0,
+                style: "border: 1px solid red",
+            },
+            templateLabel: {
+                text: this.t("templateLabel"),
+            },
+        },
     });
 
     constructor(element, history) {
@@ -47,6 +65,8 @@ class WebcPageController extends WebcController {
 
         setInterval((_) => {
             this.model.conditionResult = !this.model.conditionResult;
+            this.model.templateInput.value++;
+            this.model.templateInnerInput.templateInput.value += 2;
             this.model.button1.text = this.t(isEven ? "button1" : "button2");
             this.model.button2.text = this.t(isEven ? "button2" : "button3");
             this.model.button3.text = this.t(isEven ? "button3" : "button1");
